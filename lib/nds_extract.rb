5f93d0c5e256eee8095206aa0603b55724c62cda
William C Rutland <william.prometheus@gmail.com>
2020-01-03 04:01:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,8 +12,9 @@ def directors_totals(nds)
   while director_index < directors_database.length do
     movie_index = 0
     while movie_index < directors_database[director_index][:movies].length do
-      
-  
+      total += directors_database[director_index][:movies][movie_index][:worldwide_gross]
+    end
+    
   p result
   
   # Use loops, variables and the accessing method, [], to loop through the NDS
