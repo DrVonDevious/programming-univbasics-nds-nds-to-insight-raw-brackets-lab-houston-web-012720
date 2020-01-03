04032031844d3de415ed84f890d21087748693bf
William C Rutland <william.prometheus@gmail.com>
2020-01-03 03:57:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def directors_totals(nds)
   director_index = 0
   movie_index = 0
   
-  while director_index < directors_database.len
+  while director_index < directors_database.length
   
   p result
   
