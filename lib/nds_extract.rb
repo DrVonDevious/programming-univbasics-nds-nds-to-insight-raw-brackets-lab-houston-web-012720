1ebb75535920329dc068758669601f5595809aa9
William C Rutland <william.prometheus@gmail.com>
2020-01-03 03:56:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,9 +9,11 @@ def directors_totals(nds)
   }
   
   director_index = 0
+  movie_index = 0
   
   
-  return result
+  
+  p result
   
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the total earnings for each movie
