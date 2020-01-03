5d5613fe00de4eb2d9743e2d3975c5791cc7506f
William C Rutland <william.prometheus@gmail.com>
2020-01-03 03:46:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,9 +6,16 @@ def directors_totals(nds)
   result = {
   }
   
+  for director in directors_database do
+    for movie in directors_database[:director]:
+      total += directors_database[:director][:movie][:gross]
+    end  
+    result[:director] = total
+  end
   
-
+  return result
+  
   # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
+  # and total up all the total earnings for each movie
   
 end
